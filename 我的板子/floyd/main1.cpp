249f1f65f5e191f3fde97881cd34c9a0172f417a
pandigreat <pandigreat@163.com>
2016-11-10 20:25:14 +0800

    Add comments for floyde algorithm

--- 我的板子/floyd/main1.cpp
+++ 我的板子/floyd/main1.cpp
@@ -4,11 +4,14 @@
 /**
 四行代码，表达出递推的中心思想
 其中 mes[i][i] 为0
+未考虑回环和负数权重的版本
 **/
 
 for(int k = 0; k < n; k++)
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < n; j++)
 			mes[i][j] = min(mes[i][j], mes[i][k] + mes[k][j]);
+		
+
 		
 		
